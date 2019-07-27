f5c245407546ee09eee3bb607814032f0518739e
Kyle Johannsen <kjohannsen1122@gmail.com>
2019-07-27 15:20:53 -0400

    Created GoalCell layout and swift file. Created configureCell function.

---
+++ goalpost-app/View/GoalCell.swift
@@ -0,0 +1,25 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Kyle Johannsen on 7/27/19.
+//  Copyright © 2019 Kyle Johannsen. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    // MARK: Outlets
+    @IBOutlet weak var goalDescriptionLabel: UILabel!
+    @IBOutlet weak var goalTypeLabel: UILabel!
+    @IBOutlet weak var goalProgressLabel: UILabel!
+    
+    // MARK: Functions
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLabel.text = description
+        self.goalTypeLabel.text = type
+        self.goalProgressLabel.text = "\(goalProgressAmount)"
+    }
+
+}
